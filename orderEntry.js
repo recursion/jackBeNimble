fd06777c29be1c17e23042fb0a21cc6694e3efad
Michael Symmes <msymmes@gmail.com>
2015-11-03 19:10:14 -0500

    Now able to toggle lotsize, increment value, and place a range or orders.

--- orderEntry.js
+++ orderEntry.js
@@ -1,16 +1,20 @@
+var LOTSIZES = [0.1, 0.25, 0.5, 0.75, 1];
+var INCREMENTS = [0.01, 0.1, 0.25, 0.5, 0.75, 1];
 
-var LOTSIZE = .5;
 var INCR = 0.1;
-var smallINCR = 0.01;
-var largeINCR = 1;
+var LOTSIZE = .5;
 
 var KEYS = {
+  TOGGLE_LOTSIZE_UP: 220,
+  TOGGLE_LOTSIZE_DOWN: 222,
+  TOGGLE_INCR_UP: 189,
+  TOGGLE_INCR_DOWN: 187,
   INCR_BID: 68,
   INCR_OFFER: 75,
   OFFER_WITH_BEST_ASK: 76,
   BID_WITH_BEST_BID: 83,
   OFFER_ABOVE_BEST: 186,
-  BID_BELOW_BEST: 65
+  BID_BELOW_BEST: 65,
   MARKET_BUY: 71,
   MARKET_SELL: 72
 };
@@ -19,8 +23,8 @@ $( document ).ready(function(){
   window.addEventListener('keydown', onKeydown, false);
   window.addEventListener('keypress', onKeypress, false);
   window.addEventListener('keyup', onKeyup, false);
-  var amount = document.getElementById('amount');
-  amount.value = LOTSIZE;
+  setLotSize(LOTSIZE);
+  displayIncr(INCR);
 });
 
 function onKeydown(e){
@@ -31,13 +35,70 @@ function onKeypress(e){
   //console.log('Keyup', e);
 }
 
+function setLotSize(v){
+  v = v || LOTSIZE;
+  console.log('Setting lot size to:', v);
+  var amount = document.getElementById('amount');
+  LOTSIZE = v;
+  amount.value = v;
+}
+
+function displayIncr(v){
+  var homeDiv = $('#trading-ticket-form > div:nth-child(5) > ul > li > div.collapsible-header');
+
+  var target = $('#INCRVAL');
+  if(!target.length){
+    homeDiv.append('<span id="INCRVAL">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Increment: '+ v +'</span>');
+  } else {
+    target[0].innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Increment: ' + v;
+  }
+}
+
 function onKeyup(e){
   switch(e.keyCode){
+    case KEYS.TOGGLE_LOTSIZE_UP:
+      console.log('bing')
+      var idx = LOTSIZES.indexOf(LOTSIZE);
+      if (++idx >= LOTSIZES.length){
+        idx = 0;
+      }
+      var lotsize = LOTSIZES[idx];
+      setLotSize(lotsize);
+      break;
+
+    case KEYS.TOGGLE_LOTSIZE_DOWN:
+      var idx = LOTSIZES.indexOf(LOTSIZE);
+      if (--idx < 0){
+        idx = LOTSIZES.length - 1;
+      }
+      var lotsize = LOTSIZES[idx];
+      setLotSize(lotsize);
+      break;
+
+    case KEYS.TOGGLE_INCR_UP:
+      var idx = INCREMENTS.indexOf(INCR);
+      if (++idx >= INCREMENTS.length){
+        idx = 0;
+      }
+      INCR = INCREMENTS[idx];
+      displayIncr(INCR);
+      console.log('Setting increment to: ', INCR);
+      break;
+
+    case KEYS.TOGGLE_INCR_DOWN:
+      var idx = INCREMENTS.indexOf(INCR);
+      if (--idx < 0){
+        idx = INCREMENTS.length - 1;
+      }
+      INCR = INCREMENTS[idx];
+      displayIncr(INCR);
+      console.log('Setting increment to: ', INCR);
+      break;
 
     // place best limit bid order on the market
     case KEYS.INCR_BID:
       var bestBid = getBestBid();
-      var newBid = +bestBid + pINCR;
+      var newBid = +bestBid + INCR;
       setBid(newBid.toFixed(2));
       placeBuyOrder();
       break;
@@ -52,7 +113,7 @@ function onKeyup(e){
     // Place bid at (INCR) below the current best bid
     case KEYS.BID_BELOW_BEST:
       var bestBid = getBestBid();
-      var newBid = +bestBid - pINCR;
+      var newBid = +bestBid - INCR;
       setBid(newBid.toFixed(2));
       placeBuyOrder();
       break;
@@ -71,7 +132,7 @@ function onKeyup(e){
     // place the best limit sell on the market
     case KEYS.INCR_OFFER:
       var bestOffer = getBestOffer();
-      var newOffer = +bestOffer - pINCR;
+      var newOffer = +bestOffer - INCR;
       setAsk(newOffer.toFixed(2));
       placeSellOrder();
       break;
@@ -86,7 +147,7 @@ function onKeyup(e){
     // place offer at current best ask
     case KEYS.OFFER_ABOVE_BEST:
       var bestOffer = getBestOffer();
-      var newOffer = +bestOffer + pINCR;
+      var newOffer = +bestOffer + INCR;
       setAsk(newOffer.toFixed(2));
       placeSellOrder();
       break;
