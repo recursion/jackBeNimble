bbdc8f25ebbfd3d26a03898e5f45d705bccbdc09
Michael Symmes <msymmes@gmail.com>
2015-11-21 12:26:53 -0500

    Moved business logic out of keyboard handlers and into utils.

--- keyboardHandlers.js
+++ keyboardHandlers.js
@@ -24,42 +24,22 @@ keyboardHandlers.onKeyup = function(e){
 
     // LOTSIZE UP
     case plugin.KEYS.TOGGLE_LOTSIZE_UP:
-      var idx = plugin.LOTSIZES.indexOf(plugin.settings.LOTSIZE);
-      if (++idx >= plugin.LOTSIZES.length){
-        idx = 0;
-      }
-      var lotsize = plugin.LOTSIZES[idx];
-      utils.setLotSize(lotsize);
+      utils.toggleLotSize('up');
       break;
 
     // LOTSIZE DOWN
     case plugin.KEYS.TOGGLE_LOTSIZE_DOWN:
-      var idx = plugin.LOTSIZES.indexOf(plugin.settings.LOTSIZE);
-      if (--idx < 0){
-        idx = plugin.LOTSIZES.length - 1;
-      }
-      var lotsize = plugin.LOTSIZES[idx];
-      utils.setLotSize(lotsize);
+      utils.toggleLotSize('down');
       break;
 
       // INCREMENT UP
     case plugin.KEYS.TOGGLE_INCR_UP:
-      var idx = plugin.INCREMENTS.indexOf(plugin.settings.INCR);
-      if (++idx >= plugin.INCREMENTS.length){
-        idx = 0;
-      }
-      plugin.settings.INCR = plugin.INCREMENTS[idx];
-      utils.displayIncr(plugin.settings.INCR);
+      utils.toggleIncrement('up');
       break;
 
       // INCREMENT DOWN
     case plugin.KEYS.TOGGLE_INCR_DOWN:
-      var idx = plugin.INCREMENTS.indexOf(plugin.settings.INCR);
-      if (--idx < 0){
-        idx = plugin.INCREMENTS.length - 1;
-      }
-      plugin.settings.INCR = plugin.INCREMENTS[idx];
-      utils.displayIncr(plugin.settings.INCR);
+      utils.toggleIncrement('down');
       break;
 
 
@@ -69,33 +49,22 @@ keyboardHandlers.onKeyup = function(e){
      ********************************/
     // place best limit bid order on the market
     case plugin.KEYS.BID_BETTER:
-      var bestBid = utils.getBestBid();
-      var newBid = +bestBid + 0.01;
-      utils.setBuyPrice(newBid.toFixed(2));
-      utils.placeBuyOrder();
+      utils.bidBetter();
       break;
 
     // place bid at current best bid
     case plugin.KEYS.BID_WITH_BEST_BID:
-      var bestBid = utils.getBestBid();
-      utils.setBuyPrice(bestBid);
-      utils.placeBuyOrder();
+      utils.bidWithBest();
       break;
 
     // Place bid at (INCR) below the current best bid
     case plugin.KEYS.BID_BELOW_BEST:
-      var bestBid = utils.getBestBid();
-      var newBid = +bestBid - plugin.settings.INCR;
-      utils.setBuyPrice(newBid.toFixed(2));
-      utils.placeBuyOrder();
+      utils.bidBelowBest();
       break;
 
     // Place bid at (INCR) below the current best bid
     case plugin.KEYS.BID_DOUBLE_BELOW_BEST:
-      var bestBid = utils.getBestBid();
-      var newBid = +bestBid - (plugin.settings.INCR * 2);
-      utils.setBuyPrice(newBid.toFixed(2));
-      utils.placeBuyOrder();
+      utils.bidDoubleBelowBest();
       break;
 
     // place market buy
@@ -110,33 +79,22 @@ keyboardHandlers.onKeyup = function(e){
      ********************************/
     // place the best limit sell on the market
     case plugin.KEYS.OFFER_BETTER:
-      var bestOffer = utils.getBestOffer();
-      var newOffer = +bestOffer - 0.01;
-      utils.setSellPrice(newOffer.toFixed(2));
-      utils.placeSellOrder();
+      utils.offerBetter();
       break;
 
     // place offer at current best ask
     case plugin.KEYS.OFFER_WITH_BEST_ASK:
-      var bestOffer = utils.getBestOffer();
-      utils.setSellPrice(bestOffer);
-      utils.placeSellOrder();
+      utils.offerWithBest();
       break;
 
     // place offer at current best ask
     case plugin.KEYS.OFFER_ABOVE_BEST:
-      var bestOffer = utils.getBestOffer();
-      var newOffer = +bestOffer + plugin.settings.INCR;
-      utils.setSellPrice(newOffer.toFixed(2));
-      utils.placeSellOrder();
+      utils.offerAboveBest();
       break;
 
     // place offer at current best ask
     case plugin.KEYS.OFFER_DOUBLE_ABOVE_BEST:
-      var bestOffer = utils.getBestOffer();
-      var newOffer = +bestOffer + (plugin.settings.INCR * 2);
-      utils.setSellPrice(newOffer.toFixed(2));
-      utils.placeSellOrder();
+      utils.offerDoubleAboveBest();
       break;
 
     // place market sell
--- utils.js
+++ utils.js
@@ -1,10 +1,13 @@
+/**********************************
+ *          UTILITY FUNCTIONS
+ **********************************/
 var plugin = plugin || {};
 var utils = {};
 
-/**********************************
- *             HELPERS
- **********************************/
+
+
 /**
+ *     Set the desired lot size
  * @param {Number} v - the new lotsize value
  */
 utils.setLotSize = function(v){
@@ -15,12 +18,19 @@ utils.setLotSize = function(v){
 }
 
 /**
+ * Display the increment value
  * @param {Number} v - the new increment value
  */
 utils.displayIncr = function(v){
   plugin.strategy.displayIncr(v);
 }
 
+
+
+
+/********************************************
+ *           PLACE ORDER
+ *******************************************/
 utils.placeBuyOrder = function(){
   plugin.strategy.placeBuyOrder();
 }
@@ -29,6 +39,12 @@ utils.placeSellOrder = function(){
   plugin.strategy.placeSellOrder();
 }
 
+
+
+
+/********************************************
+ *          SET ORDER PRICE
+ *******************************************/
 utils.setBuyPrice = function(p){
   // make sure the bid is in a string format
   if(typeof p !== 'string'){
@@ -45,6 +61,12 @@ utils.setSellPrice = function(p){
   plugin.strategy.setSellPrice(p);
 }
 
+
+
+
+/****************************************
+ * Get Best Bid/ Best Offer data
+ ***************************************/
 utils.getBestBid = function(){
   return plugin.strategy.getBestBid();
 }
@@ -54,9 +76,131 @@ utils.getBestOffer = function(){
 }
 
 
+/****************************************
+ *            TOGGLES
+ ***************************************/
+/**
+ *        Toggle lot size up or down
+ * @param {String} direction - can be 'up' or 'down'
+ * determines which way to toggle the lot size
+ */
+utils.toggleLotSize = function(direction){
+    var idx = plugin.LOTSIZES.indexOf(plugin.settings.LOTSIZE);
+
+    if (direction === 'up'){
+      if (++idx >= plugin.LOTSIZES.length){
+        idx = 0;
+      }
+    } else if (direction === 'down'){
+      if (--idx < 0){
+        idx = plugin.LOTSIZES.length - 1;
+      }
+    } else {
+      console.error('Unknown lot size direction: ', direction);
+    }
+
+    var lotsize = plugin.LOTSIZES[idx];
+    utils.setLotSize(lotsize);
+}
+
+/**
+ *      Toggle the increment value up or down
+ * @param {String} direction - can be 'up' or 'down'
+ * determines which way to toggle the increment
+ */
+utils.toggleIncrement = function(direction){
+  var idx = plugin.INCREMENTS.indexOf(plugin.settings.INCR);
+  if (direction === 'up'){
+    if (++idx >= plugin.INCREMENTS.length){
+      idx = 0;
+    }
+  } else if (direction === 'down'){
+    if (--idx < 0){
+      idx = plugin.INCREMENTS.length - 1;
+    }
+  } else {
+    console.error('Unknown toggle increment direction: ', direction);
+  }
+  plugin.settings.INCR = plugin.INCREMENTS[idx];
+  utils.displayIncr(plugin.settings.INCR);
+}
+
+/*******************************************
+ *          BUY ORDERS
+ ******************************************/
+
+/* Bid just above the best bid */
+utils.bidBetter = function() {
+  var bestBid = utils.getBestBid();
+  var newBid = +bestBid + 0.01;
+  utils.setBuyPrice(newBid.toFixed(2));
+  utils.placeBuyOrder();
+}
+
+/* bid with the best current bid */
+utils.bidWithBest = function() {
+  var bestBid = utils.getBestBid();
+  utils.setBuyPrice(bestBid);
+  utils.placeBuyOrder();
+}
+
+/* bid 1 increment level below the best bid */
+utils.bidBelowBest = function(){
+  var bestBid = utils.getBestBid();
+  var newBid = +bestBid - plugin.settings.INCR;
+  utils.setBuyPrice(newBid.toFixed(2));
+  utils.placeBuyOrder();
+}
+
+/* bid 2 increment levels below best bid */
+utils.bidDoubleBelowBest = function(){
+  var bestBid = utils.getBestBid();
+  var newBid = +bestBid - (plugin.settings.INCR * 2);
+  utils.setBuyPrice(newBid.toFixed(2));
+  utils.placeBuyOrder();
+}
+
+
+
+/********************************
+ *           SELL ORDER KEYS
+ ********************************/
+
+/* Offer just below best offer */
+utils.offerBetter = function(){
+  var bestOffer = utils.getBestOffer();
+  var newOffer = +bestOffer - 0.01;
+  utils.setSellPrice(newOffer.toFixed(2));
+  utils.placeSellOrder();
+}
+
+/* Offer with the best current offer */
+utils.offerWithBest = function(){
+  var bestOffer = utils.getBestOffer();
+  utils.setSellPrice(bestOffer);
+  utils.placeSellOrder();
+}
+
+/* Offer 1 increment level above the best offer */
+utils.offerAboveBest = function(){
+  var bestOffer = utils.getBestOffer();
+  var newOffer = +bestOffer + plugin.settings.INCR;
+  utils.setSellPrice(newOffer.toFixed(2));
+  utils.placeSellOrder();
+}
+
+/* Offer 2 increment levels above the best offer */
+utils.offerDoubleAboveBest = function() {
+  var bestOffer = utils.getBestOffer();
+  var newOffer = +bestOffer + (plugin.settings.INCR * 2);
+  utils.setSellPrice(newOffer.toFixed(2));
+  utils.placeSellOrder();
+}
 /**
  * Simulate an event being fired.
  * Used for 'clicking' the buy/sell buttons.
+ * @param {Element} el - the html element we will fire the event on
+ * @param {String} - eType - the event type
  */
 utils.eventFire = function(el, etype){
   if (el.fireEvent) {
