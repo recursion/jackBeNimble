a636dbe31806e6466e64332e2805e28714958394
Michael Symmes <msymmes@gmail.com>
2016-03-19 15:18:08 -0400

    Attempting to resolve issue with plugin occasionally loading without a readied DOM.

--- src/js/jackBeNimble.js
+++ src/js/jackBeNimble.js
@@ -9,14 +9,14 @@ var plugin = plugin || {};
    * - add the default offset value to the display
    *
    ****************************************/
-  window.addEventListener('keydown', plugin.keyboardHandlers.onKeydown, false);
-  window.addEventListener('keypress', plugin.keyboardHandlers.onKeypress, false);
-  window.addEventListener('keyup', plugin.keyboardHandlers.onKeyup, false);
+    window.addEventListener('keydown', plugin.keyboardHandlers.onKeydown, false);
+    window.addEventListener('keypress', plugin.keyboardHandlers.onKeypress, false);
+    window.addEventListener('keyup', plugin.keyboardHandlers.onKeyup, false);
 
-  /**
-   * Determine the exchange interface module to use and initialize it
-   */
-  plugin.setInterface();
-  plugin.interface.init();
+    /**
+     * Determine the exchange interface module to use and initialize it
+     */
+    plugin.setInterface();
+    plugin.interface.init();
 
 })();
