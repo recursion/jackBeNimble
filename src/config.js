e8ed8d990bb2b958634b59d15d4f308d5bcde970
Michael Symmes <msymmes@gmail.com>
2016-05-10 11:28:06 -0400

    Added displayToggle option (set to true).

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const hotkeys = require('../hotkey.defaults.js')
 const hotkeyCommander = require('hotkey-commander')
 
-hotkeyCommander.Configurator({hotkeys: hotkeys, targetEl: document.getElementById('hotkeyCommander')})
+hotkeyCommander.Configurator({hotkeys: hotkeys, displayToggle: true, targetEl: document.getElementById('hotkeyCommander')})
 
 setTimeout(() => {
   window.scrollTo(0, 0)
